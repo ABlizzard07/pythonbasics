870b45893d7f3bb9e40a0717b46bdbf4196f3414
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2021-06-14 10:58:49 -0500

    Add files via upload

--- UploadFiles.py
+++ UploadFiles.py
@@ -12,8 +12,13 @@ class TransferData:
         dbx.files_upload(f.read(), file_to)
 
         for root, dirs, files in os.walk(file_from):
-            relative_path = os.path.relpath(local_path, file_from)
-            dropbox_path = os.path.join(file_to, relative_path)
+            print(root)
+            print(dirs)
+            print(files)
+            print('----------')
+        
+        relative_path = os.path.relpath(local_path, file_from)
+        dropbox_path = os.path.join(file_to, relative_path)
         
         with open(local_path, 'rb') as f:
             dbx.files.upload(f.read(), dropbox_path, mode-WriteMode('overwrite'))
